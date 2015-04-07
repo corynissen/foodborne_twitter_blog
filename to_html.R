647b9e1ea3b8ec09c2f512d5db18c5f32bb9a39b
corynissen <corynissen@gmail.com>
2015-04-07 10:12:02 -0500

    not needed

--- to_html.R
+++
@@ -1,5 +0,0 @@
-# create blog post
-library(markdown)
-library(knitr)
-knit("foodborne_twitter_blog.Rmd")
-markdownToHTML("foodborne_twitter_blog.md", "foodborne_twitter_blog.html", fragment.only = TRUE)
